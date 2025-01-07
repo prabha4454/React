d47a0b43da334d831fc7aa7e478f968075c0454b
prabha haran <hprabha72@gmail.com>
2025-01-07 12:06:11 +0530

    new changes

---
+++ A1copy/src/App.jsx
@@ -0,0 +1,15 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import  {Home} from "./components/home"
+function App() {
+ 
+
+  return (
+    <>
+      <Home></Home>
+      
+    </>
+  )
+}
+
+export default App
---
+++ A1copy/src/components/home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import "./style/home.css";
+
+export const Home = () => {
+  return (
+    <>
+      <div className="container-fluid home-section bg-danger">
+        <div className="container ">
+          <div className="home ">
+            <div className="home-left">
+              <h1 className="h1 text-white ">
+              
+                Your End-to-End IT Solutions Partner
+              </h1>
+              <p className="p text-light ">
+       
+                Welcome to A1ideaz, Where innovation meets excellence in it
+                solutions.As a leading provider of comprehensive IT services, We
+                pride ourselves on offering a diverse range of expertise to meet
+                the evolving needs of businesses across industries.
+              </p>
+
+              <button className="btn btn-outline-info ">Get Starded</button>
+            </div>
+            <div className="home-right">
+              <img src="/images/home.png" alt="" className="img-fluid" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
